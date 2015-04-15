8c5c28f4c2105e5bf56c8f6959f8c74e8f5d71a9
lizheng90 <jackyliz900215@gmail.com>
2015-04-14 20:10:21 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Zheng Li on 4/14/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
